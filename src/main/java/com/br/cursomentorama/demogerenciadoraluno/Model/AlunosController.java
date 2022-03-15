3d4d43421d45899e71fdb49b76925d0559584b6f
lino167 <zaca793@gmail.com>
2022-03-15 11:38:59 -0300

    Alterações

--- src/main/java/com/br/cursomentorama/demogerenciadoraluno/Model/AlunosController.java
+++ src/main/java/com/br/cursomentorama/demogerenciadoraluno/Model/AlunosController.java
@@ -6,19 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Stream;
-
+import java.util.stream.Collectors;
 import com.br.cursomentorama.demogerenciadoraluno.Aluno;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
